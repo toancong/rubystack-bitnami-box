13bc79e4e580ada0b72f3ea84b2d768828724d2d
toancong <toancong1920@gmail.com>
2015-11-10 22:15:07 +0700

    Add files

---
+++ Vagrantfile
@@ -0,0 +1,17 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+Vagrant.configure('2') do |config|
+  config.vm.hostname = 'localhost'
+  config.vm.box      = 'ubuntu/trusty64'
+  # config.vm.box_url  = "http://192.168.1.121/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = true
+    vb.memory = 2048
+    # vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/projects", "1"]
+  end
+
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
+
+  config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
+end
---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+echo "Starting provisioning"
+
+
+apt-get -y install build-essential
+
+
+# REM this section if you had an installer ready in root folder.
+echo "Downloading rubystack installer"
+wget https://bitnami.com/redirect/to/74445/bitnami-rubystack-2.2.3-2-dev-linux-x64-installer.run \
+                    -O /vagrant/bitnami-rubystack-2.2.3-2-dev-linux-x64-installer.run
+# wget http://192.168.1.121/bitnami-rubystack-2.2.3-2-dev-linux-x64-installer.run \
+#                     -O /vagrant/bitnami-rubystack-2.2.3-2-dev-linux-x64-installer.run
+
+echo "Running rubystack installer. It can take many minutes to install."
+/vagrant/bitnami-rubystack-2.2.3-2-dev-linux-x64-installer.run \
+                    --unattendedmodeui none --mode unattended --launchbch 0 \
+                    --postgres_password 123456 \
+                    --mysql_password 123456 \
+                    --base_password 123456
+
+
+echo "Setup startup script"
+touch /etc/init.d/rubystack
+echo "/opt/rubystack-2.2.3-2/ctlscript.sh start" > /etc/init.d/rubystack
+chmod ugo+x /etc/init.d/rubystack
+update-rc.d -f rubystack defaults
+update-rc.d -f rubystack enable
+
+
+echo "Setup path environment"
+echo ". /opt/rubystack-2.2.3-2/scripts/setenv.sh" >> /etc/bash.bashrc
+
+
+echo "Provisioning completed."
